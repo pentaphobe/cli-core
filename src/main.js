ac50683a7619d3a884217d1cc5d1ae42a6553d69
keilin.olsen <keilin@vectorflip.com>
2017-07-31 11:19:58 +1000

    adds progress bar to noop command

--- src/main.js
+++ src/main.js
@@ -7,29 +7,30 @@ const program = require('commander');
 ///////// Global configuration and options
 program
   .version(pkg.version)
-  .option('-d, --dry-run', `don't modify filesystem in any way`)
+  .option('-d, --dry-run', `don't modify filesystem in any way`);
 
 ///////// Commands
 program
   .command('noop [optional] [others...]')
   .description('does nothing, just a demonstration')
-  .action( (optional, others, program) => {
+  .action((optional, others, program) => {
     const ProgressBar = require('progress');
 
-    var bar = new ProgressBar(`${chalk.blue('doing nothing')} [:bar] :current / :total :percent`, { total: 30 });
-    var timer = setInterval(function () {
+    var bar = new ProgressBar(
+      `${chalk.blue('doing nothing')} [:bar] :current / :total :percent`,
+      { total: 30 }
+    );
+    var timer = setInterval(function() {
       bar.tick();
       if (bar.complete) {
-        console.log(chalk.green("\ncomplete\n"));
+        console.log(chalk.green('\ncomplete\n'));
         clearInterval(timer);
       }
     }, 100);
-
-  })
+  });
 
 ///////// Entry point
-program
-  .parse(process.argv)
+program.parse(process.argv);
 
 if (program.args.length === 0) {
   program.help();
